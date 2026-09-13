66663b5caf16f1089888fad4473c86e05246ac41
Chhouk SreyYing <chhouksreyying@gmail.com>
2026-09-13 15:33:07 +0700

    done

--- javascript/script.js
+++ javascript/script.js
@@ -164,6 +164,7 @@ function renderProductDetail() {
 function setupContactForm() {
   document.getElementById("contact-form").addEventListener("submit", (e) => {
     e.preventDefault();
-    location.href = "order-confirmed.html";
+    location.href = "submit-confirmed.html";
   });
 }
+
